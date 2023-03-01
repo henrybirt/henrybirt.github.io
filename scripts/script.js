1c809eea27e0ba51ca3d1ef386f667812fb6a620
Rhys Clements <rhys.clements@outlook.com>
2023-03-01 23:55:01 +0000

    Open My Eyes Overhaul
    Overhaul for song release

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,24 @@ function openNav() {
 function closeNav() {
   document.getElementById("overlay").style.display = "none";
 }
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  let title = document.getElementById("title")
+  let navlist = document.getElementById("links")
+  let menubtn = document.getElementById("menu-btn")
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    title.style.fontSize= "2.5em";
+    menubtn.style.fontSize= "2em";
+    navlist.style.fontSize= "0em";
+
+  } else {
+    title.style.fontSize= "5em";
+    menubtn.style.fontSize = "4em";
+    navlist.style.fontSize = "2em";
+
+  }
+}
